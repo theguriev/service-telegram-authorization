f0021487274226ad89244ba544188b64a10960a7
Eugen Guriev <gurievcreative@gmail.com>
2025-01-26 12:34:51 +0200

    feat: add MongoDB and mongo-express services with connection setup

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,5 +1,8 @@
 //https://nitro.unjs.io/config
 export default defineNitroConfig({
   srcDir: "server",
-  compatibilityDate: "2025-01-26"
+  compatibilityDate: "2025-01-26",
+  runtimeConfig: {
+    mongoUri: 'mongodb://root:example@localhost:27017/',
+  },
 });
