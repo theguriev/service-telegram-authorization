d583f4383eec0b2e076eadeaf471365c47550658
Eugen Guriev <gurievcreative@gmail.com>
2025-01-26 12:41:20 +0200

    feat: add user and token models with corresponding schemas and import helper

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,3 +1,6 @@
+import { camelCase } from 'scule'
+import importsHelper from './importsHelper';
+
 //https://nitro.unjs.io/config
 export default defineNitroConfig({
   srcDir: "server",
@@ -5,4 +8,18 @@ export default defineNitroConfig({
   runtimeConfig: {
     mongoUri: 'mongodb://root:example@localhost:27017/',
   },
+  imports: {
+    imports: [
+      ...(await importsHelper('./db/model')),
+      ...(await importsHelper('./db/schema', camelCase)),
+      { name: 'parse', from: 'set-cookie-parser' }
+    ],
+    presets: [
+      {
+        from: 'zod',
+        imports: ['z']
+      }
+    ],
+    dirs: ['./composables']
+  }
 });
