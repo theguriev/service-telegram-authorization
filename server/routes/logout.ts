537b42ab70cce2bdc11f409d79ac19c14bccecd0
Eugen Guriev <gurievcreative@gmail.com>
2025-02-22 20:54:33 +0200

    feat: add logout endpoint with user token deletion and response handling

---
+++ server/routes/logout.ts
@@ -0,0 +1,14 @@
+export default eventHandler(async (event) => {
+  const userId = await getUserId(event);
+  const userExist = await ModelUser.findOne({ _id: userId });
+
+  if (userExist !== null) {
+    const { deleteByUserId } = useTokens({ event, userId });
+    await deleteByUserId();
+  }
+
+  deleteCookie(event, "refreshToken");
+  deleteCookie(event, "accessToken");
+
+  return { message: "User logged out successfully" };
+});
