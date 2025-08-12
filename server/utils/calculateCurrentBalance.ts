3344f6f21ad807998f48b84fc16a36ecd383a030
biguard1 <bogdasha05@gmail.com>
2025-08-12 15:49:14 +0300

    fix: fixed daily spending

---
+++ server/utils/calculateCurrentBalance.ts
@@ -0,0 +1,49 @@
+import { differenceInDays, startOfDay } from "date-fns";
+import { sortBy } from "es-toolkit";
+
+const calculateCurrentBalance = (
+  from: string, transactions: Awaited<ReturnType<typeof getTransactions>>,
+  getStartDate: (date: Date) => Date = startOfDay
+) => {
+  const sortedTransactions = sortBy(transactions, [(transaction) => transaction.timestamp]);
+
+  const { currentBalance, previousTransaction } = sortedTransactions.reduce<{
+    previousTransaction: typeof transactions[number] | undefined;
+    currentBalance: number;
+  }>(({ previousTransaction, currentBalance }, transaction) => {
+    if (transaction.from === from) {
+      return {
+        previousTransaction: transaction,
+        currentBalance: Math.max(0, currentBalance - transaction.value),
+      };
+    }
+
+    if (!previousTransaction) {
+      return {
+        previousTransaction: transaction,
+        currentBalance: transaction.value,
+      };
+    }
+
+    const transactionDate = getStartDate(new Date(transaction.timestamp));
+    const previousTransactionDate = getStartDate(new Date(previousTransaction.timestamp));
+    const daysDifference = differenceInDays(transactionDate, previousTransactionDate);
+    const value = Math.max(0, currentBalance - daysDifference) + transaction.value;
+    return {
+      previousTransaction: transaction,
+      currentBalance: value,
+    };
+  }, {
+    previousTransaction: undefined,
+    currentBalance: 0,
+  });
+
+  const currentDate = getStartDate(new Date());
+  const previousTransactionDate = previousTransaction
+    ? getStartDate(new Date(previousTransaction.timestamp))
+    : currentDate;
+  const daysDifference = differenceInDays(currentDate, previousTransactionDate);
+  return Math.max(0, currentBalance - daysDifference);
+};
+
+export default calculateCurrentBalance;
