75e511e1f78568f953b77db1bb82430f6e0310f8
biguard1 <bogdasha05@gmail.com>
2025-08-15 15:38:09 +0300

    feat: added filters to users endpoint

--- server/routes/users/index.ts
+++ server/routes/users/index.ts
@@ -1,33 +1,167 @@
+import { startOfDay, subDays } from "date-fns";
 import { ObjectId } from "mongodb";
+import { PipelineStage } from "mongoose";
+import { z } from "zod";
+import { dateDifference } from "~~/constants";
 
 const querySchema = z.object({
-  offset: z.number().int().default(0),
-  limit: z.number().int().default(10),
+  offset: z.coerce.number().int().default(0),
+  limit: z.coerce.number().int().default(10),
   search: z.string().optional(),
+  report: z.enum(["all", "with", "without"]).default("all"),
+  measurement: z.enum(["all", "with", "without"]).default("all"),
+  status: z.enum(["all", "new", "with balance", "without balance"]).default("all"),
 });
 
-const getBaseQuery = (
+type FuncParams = z.infer<typeof querySchema> & {
   managerTelegramId: number,
   managerId: string,
   userId: string
-) => {
-  if (userId !== managerId) {
+};
+type QueryFunc = (
+  params: FuncParams
+) => PipelineStage | PipelineStage[] | Promise<PipelineStage | PipelineStage[]>;
+
+const getStartDate = (date: Date) => {
+  const startDate = startOfDay(date);
+  return new Date(startDate.getTime() + dateDifference.valueOf());
+};
+
+const queries: Record<string, QueryFunc> = {
+  getBaseQuery: ({
+    managerTelegramId,
+    managerId,
+    userId
+  }) => {
+    const baseQuery = userId !== managerId
+      ? {
+        $or: [
+          { "meta.managerId": managerTelegramId },
+          { _id: new ObjectId(managerId) },
+        ],
+      }
+      : { "meta.managerId": managerTelegramId };
+
     return {
-      $or: [
-        { "meta.managerId": managerTelegramId },
-        { _id: new ObjectId(managerId) },
-      ],
+      $match: baseQuery
     };
-  } else {
-    return { "meta.managerId": managerTelegramId };
-  }
+  },
+
+  getSearchQuery: ({ search }) => {
+    if (!search) {
+      return [];
+    }
+
+    return {
+      $match: {
+        $or: [
+          { "username": { $regex: search, $options: "i" } },
+          { "firstName": { $regex: search, $options: "i" } },
+          { "lastName": { $regex: search, $options: "i" } },
+          { "meta.firstName": { $regex: search, $options: "i" } },
+          { "meta.lastName": { $regex: search, $options: "i" } },
+        ],
+      }
+    };
+  },
+
+  getLookupMeasurementsQuery: () => [
+    {
+      $addFields: {
+        currentUserId: { $toString: "$_id" }
+      }
+    }, {
+      $lookup: {
+        from: "measurements",
+        localField: "currentUserId",
+        foreignField: "userId",
+        as: "userMeasurements",
+      },
+    }
+  ],
+
+  getReportQuery: ({ report }) => {
+    if (report === "all") {
+      return [];
+    }
+
+    const todayStart = getStartDate(new Date());
+    const yesterdayStart = subDays(todayStart, 1);
+
+    const match = {
+      createdAt: { $gte: yesterdayStart, $lt: todayStart },
+      type: "steps",
+    };
+
+    if (report === "with") {
+      return {
+        $match: {
+          userMeasurements: {
+            $elemMatch: match,
+          },
+        },
+      };
+    } else if (report === "without") {
+      return {
+        $match: {
+          userMeasurements: {
+            $not: {
+              $elemMatch: match,
+            },
+          },
+        },
+      };
+    }
+  },
+
+  getMeasurementQuery: ({ measurement }) => {
+    if (measurement === "all") {
+      return [];
+    }
+
+    const todayStart = getStartDate(new Date());
+    const yesterdayStart = subDays(todayStart, 1);
+
+    const match = {
+      createdAt: { $gte: yesterdayStart, $lt: todayStart },
+      type: { $ne: "steps" },
+    };
+
+    if (measurement === "with") {
+      return {
+        $match: {
+          userMeasurements: {
+            $elemMatch: match,
+          },
+        },
+      };
+    } else if (measurement === "without") {
+      return {
+        $match: {
+          userMeasurements: {
+            $not: {
+              $elemMatch: match,
+            },
+          },
+        },
+      };
+    }
+  },
+
+  getMeasurementProject: () => ({
+    $project: {
+      privateKey: 0,
+      userMeasurements: 0,
+      currentUserId: 0,
+    }
+  }),
 };
 
 export default defineEventHandler(async (event) => {
-  const { offset = 0, limit = 10, search } = getQuery(event);
-  const convertedOffset = Number(offset);
-  const convertedLimit = Number(limit);
-  const convertedSearch = String(search || "");
+  const validated = await zodValidateData(
+    getQuery(event),
+    querySchema.parse
+  );
   const role = await getUserRole(event);
   const initialId = await getId(event);
   const userId = await getUserId(event);
@@ -40,7 +174,7 @@ export default defineEventHandler(async (event) => {
   }
 
   const manager = await ModelUser.findOne({
-    _id: new ObjectId(initialId),
+    _id: new ObjectId(initialId as string),
   });
 
   if (!manager) {
@@ -50,33 +184,59 @@ export default defineEventHandler(async (event) => {
     });
   }
 
-  await zodValidateData(
-    {
-      offset: convertedOffset,
-      limit: convertedLimit,
-    },
-    querySchema.parse
+  const params = {
+    ...validated,
+    managerId: manager._id.toString(),
+    managerTelegramId: manager.id,
+    userId: userId.toString(),
+  };
+
+  const asyncAggregateQueries = Object.values(queries).map(async (query) => {
+    const result = query(params);
+
+    if (result instanceof Promise) {
+      return await result;
+    }
+
+    return result;
+  });
+  const aggregateQueries = await Promise.all(asyncAggregateQueries);
+  const aggregateQuery = aggregateQueries.reduce<PipelineStage[]>((acc, query) =>
+    Array.isArray(query)
+      ? [...acc, ...query]
+      : [...acc, query],
+    []
   );
 
-  const baseQuery = getBaseQuery(manager.id, String(manager._id), userId);
+  const data = await ModelUser.aggregate(aggregateQuery);
 
-  if (!convertedSearch) {
-    return ModelUser.find(baseQuery)
-      .skip(convertedOffset)
-      .limit(convertedLimit);
+  const { status, offset, limit } = validated;
+  if (status === "all") {
+    return data.slice(offset, offset + limit);
   }
 
-  return ModelUser.find({
-    $and: [
-      baseQuery,
-      {
-        $or: [
-          { firstName: { $regex: new RegExp(convertedSearch, "i") } },
-          { lastName: { $regex: new RegExp(convertedSearch, "i") } },
-        ],
-      },
-    ],
-  })
-    .skip(convertedOffset)
-    .limit(convertedLimit);
+  const asyncTransformedData = data.map(async (item) => {
+    const wallet = await ModelWallet.findOne({
+      userId: item._id,
+    });
+    const balance = await getBalance(wallet.privateKey);
+    const transaction = await getTransactions(wallet.privateKey, { limit: 1 });
+    return {
+      ...item,
+      balance,
+      containsTransactions: transaction.length > 0,
+    };
+  });
+  const transformedData = await Promise.all(asyncTransformedData);
+  const filteredData = transformedData.filter((item) => {
+    if (status === "new") {
+      return !item.balance && !item.containsTransactions;
+    }
+    if (status === "with balance") {
+      return item.balance > 0;
+    }
+    return !item.balance;
+  }).map(item => omit(item, ['balance', 'containsTransactions']));
+
+  return filteredData.slice(offset, offset + limit);
 });
