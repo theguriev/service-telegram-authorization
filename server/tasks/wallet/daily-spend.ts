f5fa2d6d18c08609ecae7c984e1a4025091455df
biguard1 <bogdasha05@gmail.com>
2025-09-04 02:15:04 +0300

    fix: fixed manager retrieving

--- server/tasks/wallet/daily-spend.ts
+++ server/tasks/wallet/daily-spend.ts
@@ -51,11 +51,11 @@ export default defineTask({
     ]);
 
     const retrieveStartDate = (date: Date) => addHours(startOfDay(date), date.getHours() >= 21 ? 21 : -3);
-    for (const { privateKey, userId, users, managers } of wallets) {
+    for (const { privateKey, userId, users } of wallets) {
       try {
         const balance = await getBalance(privateKey);
         const user = users[0];
-        const manager = managers[0];
+        const manager = user.managers[0];
         if (!manager) {
           console.warn(`Manager not found for user ${userId}`);
           continue;
