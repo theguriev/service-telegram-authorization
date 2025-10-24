50741dbd8c2277a700ea794802b5d58acecccc08
Eugen Guriev <gurievcreative@gmail.com>
2025-10-24 18:27:39 +0300

    fix: refactor login logic to improve error handling and streamline user creation process

--- server/routes/login/index.post.ts
+++ server/routes/login/index.post.ts
@@ -9,68 +9,34 @@ const requestBodySchema = z.object({
 });
 
 export default eventHandler(async (event) => {
-  const { botToken } = useRuntimeConfig();
-  const { id, authDate, firstName, hash, lastName, photoUrl, username } =
-    await zodValidateBody(event, requestBodySchema.parse);
-  console.log("log: trying to login via login ", {
-    id,
-    authDate,
-    firstName,
-    lastName,
-    photoUrl,
-    username,
-  });
-
-  const valid = isValidTelegramHash(
-    { id, firstName, lastName, username, photoUrl, authDate, hash },
-    botToken
-  );
-  console.log("log: 1 ");
-  if (!valid) {
-    throw createError({ message: "Invalid user hash!", status: 403 });
-  }
-  console.log("log: 2 ");
-  const userRecord = await ModelUser.findOne({ id });
-  console.log("log: 3 ");
-  if (userRecord === null) {
-    console.log("log: 4 ");
-    const wallet = Wallet.createRandom();
-    const userDocument = new ModelUser({
+  try {
+    const { botToken } = useRuntimeConfig();
+    const { id, authDate, firstName, hash, lastName, photoUrl, username } =
+      await zodValidateBody(event, requestBodySchema.parse);
+    console.log("log: trying to login via login ", {
       id,
       authDate,
       firstName,
-      hash,
       lastName,
       photoUrl,
       username,
-      privateKey: wallet.privateKey,
-      address: wallet.address,
-      permissions: [],
-      featureFlags: ["ffMealsV2"],
-      meta: {},
     });
-    const userSaved = await userDocument.save();
-    console.log("log: 4 --");
-    const userId = userSaved._id.toString();
-    const role = userSaved.role || "user";
-    const { save } = useTokens({
-      event,
-      userId,
-      role,
-    });
-    save();
 
-    return userDocument;
-  }
-  const _id = userRecord._id.toString();
-  const role = userRecord.role || "user";
-  console.log("log: 5 ");
-  await ModelUser.updateOne(
-    {
-      _id,
-    },
-    {
-      $set: {
+    const valid = isValidTelegramHash(
+      { id, firstName, lastName, username, photoUrl, authDate, hash },
+      botToken
+    );
+    console.log("log: 1 ");
+    if (!valid) {
+      throw createError({ message: "Invalid user hash!", status: 403 });
+    }
+    console.log("log: 2 ");
+    const userRecord = await ModelUser.findOne({ id });
+    console.log("log: 3 ");
+    if (userRecord === null) {
+      console.log("log: 4 ");
+      const wallet = Wallet.createRandom();
+      const userDocument = new ModelUser({
         id,
         authDate,
         firstName,
@@ -78,17 +44,56 @@ export default eventHandler(async (event) => {
         lastName,
         photoUrl,
         username,
-      },
+        privateKey: wallet.privateKey,
+        address: wallet.address,
+        permissions: [],
+        featureFlags: ["ffMealsV2"],
+        meta: {},
+      });
+      const userSaved = await userDocument.save();
+      console.log("log: 4 --");
+      const userId = userSaved._id.toString();
+      const role = userSaved.role || "user";
+      const { save } = useTokens({
+        event,
+        userId,
+        role,
+      });
+      save();
+
+      return userDocument;
     }
-  );
-  // const { save } = useTokens({
-  //   event,
-  //   userId: _id,
-  //   role,
-  //   id: _id,
-  // });
-  // console.log("log: 6 ");
-  // save();
-  console.log("log: 7 ");
-  return ModelUser.findOne({ _id });
+    const _id = userRecord._id.toString();
+    const role = userRecord.role || "user";
+    console.log("log: 5 ");
+    await ModelUser.updateOne(
+      {
+        _id,
+      },
+      {
+        $set: {
+          id,
+          authDate,
+          firstName,
+          hash,
+          lastName,
+          photoUrl,
+          username,
+        },
+      }
+    );
+    // const { save } = useTokens({
+    //   event,
+    //   userId: _id,
+    //   role,
+    //   id: _id,
+    // });
+    // console.log("log: 6 ");
+    // save();
+    console.log("log: 7 ");
+    return ModelUser.findOne({ _id });
+  } catch (error) {
+    console.error("Error occurred during login:", error);
+    throw createError({ message: "Internal Server Error", status: 500 });
+  }
 });
--- server/routes/login/web-app.post.ts
+++ server/routes/login/web-app.post.ts
@@ -15,119 +15,126 @@ const requestBodySchema = z.object({
 });
 
 export default eventHandler(async (event) => {
-  const { botToken } = useRuntimeConfig();
-  const validated = await zodValidateBody(event, requestBodySchema.parse);
-  const valid = isValidTelegramHash(validated, botToken, true);
-
-  const { user, receiver, authDate, hash } = validated;
-  console.log("log: trying to login via web app ", {
-    user,
-    receiver,
-    authDate,
-    hash,
-  });
-  if (!user && !receiver) {
-    throw createError({
-      message: "Either 'user' or 'receiver' must be provided.",
-      status: 400,
-    });
-  }
-  const {
-    id,
-    first_name: firstName,
-    last_name: lastName,
-    username,
-    photo_url: photoUrl,
-  } = destr<{
-    id: number;
-    first_name: string;
-    last_name: string;
-    username: string;
-    language_code: string;
-    allows_write_to_pm: boolean;
-    photo_url: string;
-  }>(user ?? receiver);
-
-  if (!valid) {
-    throw createError({ message: "Invalid user hash!", status: 403 });
-  }
-  console.log("log: 1 ");
-  const userRecord = await ModelUser.findOne({ id });
-  console.log("log: 2 ");
-  if (userRecord === null) {
-    console.log("log: 3 ");
-    const wallet = Wallet.createRandom();
-    const userDocument = new ModelUser({
-      id,
-      authDate,
-      firstName,
-      hash,
-      lastName,
-      photoUrl,
-      username,
-      privateKey: wallet.privateKey,
-      address: wallet.address,
-      permissions: [],
-      featureFlags: ["ffMealsV2"],
-      meta: {},
-    });
-    console.log("log: 4 ");
-    const userSaved = await userDocument.save();
-    console.log("log: 5 ");
-    const userId = userSaved._id.toString();
-    const role = userSaved.role || "user";
-    const { save } = useTokens({
-      event,
-      userId,
-      role,
-    });
-    save();
-
-    return userDocument;
-  }
-  const _id = userRecord._id.toString();
-  const role = userRecord.role || "user";
-  console.log("log: 6 ");
-  // await ModelUser.updateOne(
-  //   {
-  //     _id,
-  //   },
-  //   {
-  //     $set: {
-  //       id,
-  //       authDate,
-  //       firstName,
-  //       hash,
-  //       lastName,
-  //       photoUrl,
-  //       username,
-  //     },
-  //   }
-  // );
   try {
-    await ModelUser.updateOne(
-      { _id },
-      { $set: { id, authDate, firstName, hash, lastName, photoUrl, username } }
-    );
-    console.log("updateOne done");
-  } catch (err) {
-    console.error("updateOne error:", err);
+    const { botToken } = useRuntimeConfig();
+    const validated = await zodValidateBody(event, requestBodySchema.parse);
+    const valid = isValidTelegramHash(validated, botToken, true);
+
+    const { user, receiver, authDate, hash } = validated;
+    console.log("log: trying to login via web app ", {
+      user,
+      receiver,
+      authDate,
+      hash,
+    });
+    if (!user && !receiver) {
+      throw createError({
+        message: "Either 'user' or 'receiver' must be provided.",
+        status: 400,
+      });
+    }
+    const {
+      id,
+      first_name: firstName,
+      last_name: lastName,
+      username,
+      photo_url: photoUrl,
+    } = destr<{
+      id: number;
+      first_name: string;
+      last_name: string;
+      username: string;
+      language_code: string;
+      allows_write_to_pm: boolean;
+      photo_url: string;
+    }>(user ?? receiver);
+
+    if (!valid) {
+      throw createError({ message: "Invalid user hash!", status: 403 });
+    }
+    console.log("log: 1 ");
+    const userRecord = await ModelUser.findOne({ id });
+    console.log("log: 2 ");
+    if (userRecord === null) {
+      console.log("log: 3 ");
+      const wallet = Wallet.createRandom();
+      const userDocument = new ModelUser({
+        id,
+        authDate,
+        firstName,
+        hash,
+        lastName,
+        photoUrl,
+        username,
+        privateKey: wallet.privateKey,
+        address: wallet.address,
+        permissions: [],
+        featureFlags: ["ffMealsV2"],
+        meta: {},
+      });
+      console.log("log: 4 ");
+      const userSaved = await userDocument.save();
+      console.log("log: 5 ");
+      const userId = userSaved._id.toString();
+      const role = userSaved.role || "user";
+      const { save } = useTokens({
+        event,
+        userId,
+        role,
+      });
+      save();
+
+      return userDocument;
+    }
+    const _id = userRecord._id.toString();
+    const role = userRecord.role || "user";
+    console.log("log: 6 ");
+    // await ModelUser.updateOne(
+    //   {
+    //     _id,
+    //   },
+    //   {
+    //     $set: {
+    //       id,
+    //       authDate,
+    //       firstName,
+    //       hash,
+    //       lastName,
+    //       photoUrl,
+    //       username,
+    //     },
+    //   }
+    // );
+    try {
+      await ModelUser.updateOne(
+        { _id },
+        {
+          $set: { id, authDate, firstName, hash, lastName, photoUrl, username },
+        }
+      );
+      console.log("updateOne done");
+    } catch (err) {
+      console.error("updateOne error:", err);
+    }
+    // const { save } = useTokens({
+    //   event,
+    //   userId: _id,
+    //   role,
+    //   id: _id,
+    // });
+    // try {
+    //   const { save } = useTokens({ event, userId: _id, role, id: _id });
+    //   await save();
+    //   console.log("tokens saved");
+    // } catch (err) {
+    //   console.error("useTokens.save error:", err);
+    // }
+    console.log("log: 7 ");
+    // save();
+    console.log("log: 8 ");
+    return ModelUser.findOne({ _id });
+  } catch (error) {
+    console.error("Error occurred during login:", error);
+    throw createError({ message: "Internal Server Error", status: 500 });
   }
-  // const { save } = useTokens({
-  //   event,
-  //   userId: _id,
-  //   role,
-  //   id: _id,
-  // });
-  // try {
-  //   const { save } = useTokens({ event, userId: _id, role, id: _id });
-  //   await save();
-  //   console.log("tokens saved");
-  // } catch (err) {
-  //   console.error("useTokens.save error:", err);
-  // }
-  console.log("log: 7 ");
-  // save();
-  console.log("log: 8 ");
-  return ModelUser.findOne({ _id });
 });
